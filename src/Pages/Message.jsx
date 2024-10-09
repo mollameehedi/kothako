f3899ffcc8e9b09c9eb9aaf8777a01f22d4d316c
molla mehedi <mollameehedi@gmail.com>
2024-10-10 02:55:08 +0600

    mobile responsive done

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -5,10 +5,10 @@ import ChatBox from '../Components/Screen/ChatBox'
 const Message = () => {
   return (
     <div className='flex h-full'>
-      <div className='w-5/12 h-full'>
+      <div className='lg:w-5/12 w-4/12 h-full'>
       <ChatList/>
       </div>
-      <div className='w-7/12 h-full'>
+      <div className='lg:w-7/12  w-8/12 h-full'>
       <ChatBox/>
         
       </div>
